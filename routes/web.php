7b2e843895b6a2b5ad38e2ec7874f57aadd629c3
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-07-21 03:43:21 +0300

    add midlleware to homepage

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     // return view('welcome');
     return view('employees.index');
-});
+})->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
